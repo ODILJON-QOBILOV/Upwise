0fb97d4aeaa2d7fa98688a9ae5ef841dae18157a
xasan <fatxuddinovx@gmail.com>
2025-03-28 17:26:45 +0500

    modelar yozildi

--- apps/admins/models.py
+++ apps/admins/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from apps.teachers.models import BaseModel
 # Create your models here.
+
+class QuizDirection(BaseModel):
+    name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.name
+
+class Test(BaseModel):
+    class Degree(models.TextChoices):
+        EASY = 'easy'
+        MEDIUM = 'medium'
+        HARD = 'hard'
+
+    name = models.CharField(max_length=55)
+    degree = models.CharField(max_length=55, choices=Degree.choices, default=Degree.MEDIUM)
+    quiz_direction = models.ForeignKey(QuizDirection, on_delete=models.CASCADE, related_name="tests")
+
+    def __str__(self):
+        return self.name
+
+class Question(BaseModel):
+    question = models.CharField(max_length=255)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name="questions")
+
+    def __str__(self):
+        return self.question
+
+class Option(BaseModel):
+    option = models.BooleanField(default=False)
+    text = models.CharField(max_length=55)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="options")
+
+    def __str__(self):
+        return self.option
--- apps/students/models/user.py
+++ apps/students/models/user.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import TextChoices
 
-class User(AbstractUser):
+from apps.teachers.models import BaseModel
+
+
+class UserType(models.TextChoices):
+    STUDENT = 'STUDENT'
+    TEACHER = 'TEACHER'
+    ADMIN = 'ADMIN'
+
+
+class User(AbstractUser, BaseModel):
     image = models.ImageField(upload_to='users/images/%Y/%m/%d', blank=True, null=True)
     phone = models.IntegerField(blank=True, null=True)
     email = models.CharField(unique=True, max_length=255)
+    user_type = models.CharField(max_length=10, choices=UserType.choices, default=UserType.STUDENT)
 
